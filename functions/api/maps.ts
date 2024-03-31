76765c2459b63c78d3dc708e6a480e0605180741
Syuks <nicoquattrochi@hotmail.com>
2024-03-31 05:16:11 -0300

    Test cf env vars

--- functions/api/maps.ts
+++ functions/api/maps.ts
@@ -1,8 +1,20 @@
 interface Env {
     KZPROFILE: KVNamespace
+    CF_PAGES: string
+    CF_PAGES_COMMIT_SHA: string
+    CF_PAGES_BRANCH: string
+    CF_PAGES_URL: string
 }
 
 export const onRequest: PagesFunction<Env> = async ({ env }) => {
-    const maps = await env.KZPROFILE.get("maps")
-    return new Response(JSON.stringify(maps))
+    return new Response(
+        JSON.stringify({
+            CF_PAGES: env.CF_PAGES || "",
+            CF_PAGES_COMMIT_SHA: env.CF_PAGES_COMMIT_SHA || "",
+            CF_PAGES_BRANCH: env.CF_PAGES_BRANCH || "",
+            CF_PAGES_URL: env.CF_PAGES_URL || "",
+        }),
+    )
+    /*const maps = await env.KZPROFILE.get("maps")
+    return new Response(JSON.stringify(maps))*/
 }
