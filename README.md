b34f35d87c0977e820730009ce257b26c6e50733
Syuks <nicoquattrochi@hotmail.com>
2026-07-01 15:59:09 -0300

    Change stack to vite-plus and cloudflare workers with the cf vite plugin

--- README.md
+++ README.md
@@ -10,17 +10,8 @@ Then set your `STEAM_API_KEY` environment variable in your Cloudflare dashboard 
 
 Create a KV namespace in your Cloudflare dashboard and bind it to your Cloudflre Page with the binding name `KZPROFILE`.
 
-Create a `wrangler.toml` file in the root of the respository and add your KV binding like this:
-
-```js
-kv_namespaces = [
-  { binding = "KZPROFILE", id = "<YOUR KV NAMESPACE ID>", preview_id = "<YOUR KV NAMESPACE ID>" },
-]
-```
-
-In Cloudflare Pages, local development uses local storage. It cannot access data stored on Cloudflare’s servers.
-KV must be seeded separetly for local development. See `functions/api/maps.ts`
+Change the ID of the binding inside `wrangler.jsonc`.
 
 ## Run
 
-To start the development server run `npx wrangler pages dev -- npm run dev`
+To start the development server run `vp run dev`.
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,41 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite-plus";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+  fmt: {
+    semi: false,
+    sortImports: true,
+    sortTailwindcss: { functions: ["cn"] },
+    ignorePatterns: [
+      "**/components/ui",
+      "pnpm-lock.yaml",
+    ],
+  },
+  lint: {
+    plugins: ["react", "typescript", "oxc"],
+    rules: {
+      "react/rules-of-hooks": "error",
+      "react/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
         },
+      ],
+      "vite-plus/prefer-vite-plus-imports": "error",
     },
-})
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+    jsPlugins: [
+      {
+        name: "vite-plus",
+        specifier: "vite-plus/oxlint-plugin",
+      },
+    ],
+  },
+  resolve: { tsconfigPaths: true },
+  plugins: [react(), cloudflare()],
+});
